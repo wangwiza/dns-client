1f099ea6132392aba324bfa28b33327af1e4d8e3
Kevin Li <kevin-ruikai.li@mail.mcgill.ca>
2024-09-26 14:23:12 -0400

    added input validation for server

--- dnsClient.py
+++ dnsClient.py
@@ -1,8 +1,10 @@
 ''' DNS Client (command line application)
 Authors: William Wang 261113954
-         Kevin-RuiKai Li  
+         Kevin-Ruikai Li 261120382 
 '''
 import argparse
+import sys
+import socket
 
 
 # Is invoked from the command line (STDIN);
@@ -19,10 +21,22 @@ parser.add_argument('-p', '--port', type=int, default=53, help='Port number')
 parser.add_argument('-mx', '--mx', action='store_true', help='MX flag')
 parser.add_argument('-ns', '--ns', action='store_true', help='NS flag')
 parser.add_argument('server', type=str, help='Server name')
+parser.add_argument('name', type=str, help='Name')
 
 args = parser.parse_args()
-print(args.mx)
+
+if args.server[0] != '@':
+    print('Error: Server name must start with @')
+    sys.exit(1)
+
+server = args.server[1:]
+if server.split('.').count != 4 or not all(0 <= int(x) < 256 for x in server.split('.')):
+    print('Error: Server name must be in the format of an IP address (x.x.x.x) and each x must be between 0 and 255')
+    sys.exit(1)
+
 # Sends a query to the server for the given domain name using a UDP socket;
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Waits for the response to be returned from the server;
 
